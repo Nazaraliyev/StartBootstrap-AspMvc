02700e71638cecd8df108e383349a63df1328885
Nazaralieff <67953949+Nazaralieff@users.noreply.github.com>
2022-01-04 13:27:43 +0400

    Created AppDbContext

---
+++ StartBootstrap-Asp/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StartBootstrap_Asp.Models;
+
+namespace StartBootstrap_Asp.Data
+{
+	public class AppDbContext:DbContext
+	{
+		public AppDbContext(DbContextOptions options) : base(options){ }
+
+
+		public DbSet<Settings> settings { get; set; }
+		public DbSet<SosialMedia> sosialMedias { get; set; }
+		public DbSet<ContactMessage> contactMessages { get; set; }
+	}
+}
