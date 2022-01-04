d5ad554f1aae5145b0d169599994f303eecd3db0
Nazaralieff <67953949+Nazaralieff@users.noreply.github.com>
2022-01-04 22:54:44 +0400

    Admin Panel SosialMedia Crud is Ready

--- StartBootstrap-Asp/Areas/Admin/Controllers/SettingsController.cs
+++ StartBootstrap-Asp/Areas/Admin/Controllers/SettingsController.cs
@@ -1,13 +1,89 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using StartBootstrap_Asp.Data;
+using StartBootstrap_Asp.Models;
+using StartBootstrap_Asp.ViewModel;
+using System.Linq;
 
 namespace StartBootstrap_Asp.Areas.Admin.Controllers
 {
     [Area("admin")]
     public class SettingsController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public SettingsController(AppDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
+            VmSettings model = new VmSettings()
+            {
+                settings = _context.settings.FirstOrDefault(),
+                sosialMedias = _context.sosialMedias.ToList(),
+            };
+            return View(model);
+        }
+
+
+
+
+
+
+
+
+        public IActionResult SmCreate()
+        {
             return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult SmCreate(SosialMedia model)
+        {
+            if (model.Icon != null && model.Name != null && model.Link != null)
+            {
+                _context.sosialMedias.Add(model);
+                _context.SaveChanges();
+                return RedirectToAction("index");
+            }
+            else
+            {
+                ModelState.AddModelError("", "All box is requared");
+                return View(model);
+            }
+        }
+
+
+
+
+        public IActionResult SmUpdate(int Id)
+        {
+            return View(_context.sosialMedias.FirstOrDefault(s => s.Id == Id));
+        }
+
+
+        [HttpPost]
+        public IActionResult SmUpdate(SosialMedia model)
+        {
+            if (model.Icon != null && model.Name != null && model.Link != null)
+            {
+                _context.sosialMedias.Update(model);
+                _context.SaveChanges();
+                return RedirectToAction("index");
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+
+
+        public IActionResult SmDelete(int Id)
+        {
+            _context.sosialMedias.Remove(_context.sosialMedias.FirstOrDefault(s => s.Id == Id));
+            _context.SaveChanges();
+            return RedirectToAction("index");
         }
     }
 }
---
+++ StartBootstrap-Asp/ViewModel/VmSettings.cs
@@ -0,0 +1,11 @@
+﻿using StartBootstrap_Asp.Models;
+using System.Collections.Generic;
+
+namespace StartBootstrap_Asp.ViewModel
+{
+    public class VmSettings
+    {
+        public List<SosialMedia> sosialMedias { get; set; }
+        public Settings settings { get; set; }
+    }
+}
