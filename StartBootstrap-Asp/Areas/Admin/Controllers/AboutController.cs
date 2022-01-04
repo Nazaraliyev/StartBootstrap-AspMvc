5aa7bf16c3e70e15ee184fc5bbcd8f8aae2a3544
Nazaralieff <67953949+Nazaralieff@users.noreply.github.com>
2022-01-04 21:08:46 +0400

    Admin Panel About Crud Is done

--- StartBootstrap-Asp/Areas/Admin/Controllers/AboutController.cs
+++ StartBootstrap-Asp/Areas/Admin/Controllers/AboutController.cs
@@ -1,14 +1,43 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using StartBootstrap_Asp.Data;
+using StartBootstrap_Asp.Models;
+using System.Linq;
 
 namespace StartBootstrap_Asp.Areas.Admin.Controllers
 {
     [Area("admin")]
     public class AboutController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public AboutController(AppDbContext context)
+        {
+            _context = context;
+        }
         
         public IActionResult Index()
         {
-            return View();
+            return View(_context.settings.FirstOrDefault());
+        }
+
+        public IActionResult Update()
+        {
+            return View(_context.settings.FirstOrDefault());
+        }
+
+        [HttpPost]
+        public IActionResult Update(Settings model)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.settings.FirstOrDefault().AboutText = model.AboutText;
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(model);
+            }
         }
     }
 }
--- StartBootstrap-Asp/Areas/Admin/Controllers/BannerController.cs
+++ StartBootstrap-Asp/Areas/Admin/Controllers/BannerController.cs
@@ -1,13 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using StartBootstrap_Asp.Data;
+using StartBootstrap_Asp.Models;
+using System.Linq;
 
 namespace StartBootstrap_Asp.Areas.Admin.Controllers
 {
     [Area("admin")]
     public class BannerController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public BannerController(AppDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            return View(_context.settings.FirstOrDefault());
+        }
+
+        public IActionResult Update()
+        {
+            return View(_context.settings.FirstOrDefault());
+        }
+
+
+        [HttpPost]
+        public IActionResult Update(Settings model)
+        {
+            if (ModelState.IsValid)
+            {
+                return View(model);
+            }
+            else
+            {
+                return View(model);
+            }
         }
     }
 }
