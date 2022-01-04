87baaedb3fca848187f7c68a66fe933518a35985
Nazaralieff <67953949+Nazaralieff@users.noreply.github.com>
2022-01-04 14:07:21 +0400

    Created Viewmodel folder and VmHome Class

---
+++ StartBootstrap-Asp/ViewModel/VmHome.cs
@@ -0,0 +1,12 @@
+﻿using StartBootstrap_Asp.Models;
+using System.Collections.Generic;
+
+namespace StartBootstrap_Asp.ViewModel
+{
+	public class VmHome
+	{
+		public Settings settings { get; set; }
+		public List<SosialMedia> sosialMedia { get; set; }
+		public List<Portfolio> portfolios { get; set; }
+	}
+}
