1363fe90a7c28bed108bd92acab6353815164a42
Nazaralieff <67953949+Nazaralieff@users.noreply.github.com>
2022-01-04 14:08:06 +0400

    Created Model with VmHome Class in HomeController.cs

--- StartBootstrap-Asp/Controllers/HomeController.cs
+++ StartBootstrap-Asp/Controllers/HomeController.cs
@@ -1,12 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using StartBootstrap_Asp.Data;
+using StartBootstrap_Asp.ViewModel;
+using System.Linq;
 
 namespace StartBootstrap_Asp.Controllers
 {
     public class HomeController : Controller
     {
+		private readonly AppDbContext _context;
+
+		public HomeController(AppDbContext context)
+		{
+			_context = context;
+		}
         public IActionResult Index()
         {
-            return View();
+            VmHome model = new VmHome()
+            {
+                sosialMedia = _context.sosialMedias.ToList(),
+                portfolios = _context.portfolios.ToList(),
+                settings = _context.settings.FirstOrDefault()
+
+            };
+            return View(model);
         }
     }
 }
